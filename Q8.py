be3b16c255fb313a805973454551664d2036f257
unknown <C110156239@nkust.edu.tw>
2022-03-28 18:07:22 +0800

    asdfsd

--- Q8.py
+++ Q8.py
@@ -1,15 +1,4 @@
-while True:
-    tr = input("請輸入要檢測的字串(end結束):")
-    print("要檢測的字串:",tr)
+while true:
+    m = int(input("輸入第一行正整數為:"))
 
-    if tr == "end":
-        print("檢測結束")
-        break
-
-    yu = input("請輸入要檢測單一的字元(end結束):")
-    if yu == "end":
-        print("檢測結束")
-        break
-
-    co = tr.count(yu)
-    print("字元",yu,"出現次數為",co)
+    
